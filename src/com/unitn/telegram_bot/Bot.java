d454f09d1a883e066cbf2b5fb644d7807bd5e19d
Erinda <erinda.jaupaj@u-hopper.com>
2016-02-02 23:22:22 +0100

    insert steps and validate input

--- src/com/unitn/telegram_bot/Bot.java
+++ src/com/unitn/telegram_bot/Bot.java
@@ -175,6 +175,7 @@ public class Bot extends TelegramBot {
                         return state.next(UserStates.SAVE_STEPS);
                 }
             case SAVE_STEPS:
+                int steps = validateSteps(text); //TODO
                 return state.next(UserStates.IDLE);
             case SAVE_GOAL:
                 Goal g = validateGoal(cmd);
@@ -237,6 +238,7 @@ public class Bot extends TelegramBot {
                 args = oneTimeKeyboard("STEPS", "GOAL", "CANCEL");
                 break;
             case SAVE_STEPS:
+                text = "Insert how many steps you did today:";
                 break;
             case SAVE_GOAL:
                 text = "Which goal do you want to set?";
